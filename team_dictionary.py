e29d6b76549175350463ce6286ec1858fdedae08
Greg Black <77752210+gregblack90@users.noreply.github.com>
2022-03-24 18:19:52 -0400

    Error checking
    Error checking added to prevent issues when there are no prospects for selected team or no game logs for selected player.

--- team_dictionary.py
+++ team_dictionary.py
@@ -2,7 +2,7 @@
 team_dict = {"Anaheim Ducks": 24, "Arizona Coyotes": 53, "Boston Bruins": 6, "Buffalo Sabres": 7,
              "Calgary Flames": 20, "Carolina Hurricanes": 12, "Chicago Blackhawks": 16, "Colorado Avalanche": 21,
              "Columbus Blue Jackets": 29, "Dallas Stars": 25, "Detroit Red Wings": 17, "Edmonton Oilers": 22,
-             "Florida Panthers": 13, "Los Angeles Kings": 26, "Minnesota Wild": 30, "Montréal Canadiens": 8,
+             "Florida Panthers": 13, "Los Angeles Kings": 26, "Minnesota Wild": 30, "Montreal Canadiens": 8,
              "Nashville Predators": 18, "New Jersey Devils": 1, "New York Islanders": 2, "New York Rangers": 3,
              "Ottawa Senators": 9, "Philadelphia Flyers": 4, "Pittsburgh Penguins": 5, "San Jose Sharks": 28,
              "Seattle Kraken": 55, "St. Louis Blues": 19, "Tampa Bay Lightning": 14, "Toronto Maple Leafs": 10,
